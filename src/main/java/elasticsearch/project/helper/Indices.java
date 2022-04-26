3ded366b2a18a9094848567b8276d65d5ceda8cc
Igor <igor.ptz@hotmail.com>
2022-04-25 23:20:09 -0300

    Update

--- src/main/java/elasticsearch/project/helper/Indices.java
+++ src/main/java/elasticsearch/project/helper/Indices.java
@@ -3,4 +3,6 @@ package elasticsearch.project.helper;
 public final class Indices {
 	
 	public static final String PERSON_INDEX = "person";
+	
+	public static final String VEHICLE_INDEX = "vehicle";
 }
