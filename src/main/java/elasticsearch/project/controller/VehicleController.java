0710fd035180f98b36e3691b4cee16b5769564df
Igor <igor.ptz@hotmail.com>
2022-04-27 22:45:05 -0300

    Update

--- src/main/java/elasticsearch/project/controller/VehicleController.java
+++ src/main/java/elasticsearch/project/controller/VehicleController.java
@@ -1,5 +1,7 @@
 package elasticsearch.project.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import elasticsearch.project.document.Vehicle;
+import elasticsearch.project.search.SearchRequestDTO;
 import elasticsearch.project.service.VehicleService;
 
 @RestController
@@ -30,5 +33,10 @@ public class VehicleController {
 	@GetMapping("/{id}")
 	public Vehicle getById(@PathVariable final String id) {
 		return service.getById(id);
+	}
+	
+	@PostMapping("/search")
+	public List<Vehicle> search(@RequestBody final SearchRequestDTO dto) {
+		return service.search(dto);
 	}
 }
--- src/main/java/elasticsearch/project/document/Vehicle.java
+++ src/main/java/elasticsearch/project/document/Vehicle.java
@@ -1,10 +1,16 @@
 package elasticsearch.project.document;
 
+import java.util.Date;
+
 public class Vehicle {
 	
 	private String id;
 	
 	private String number;
+	
+	private String name;
+	
+	private Date created;
 
 	public String getId() {
 		return id;
@@ -20,5 +26,21 @@ public class Vehicle {
 
 	public void setNumber(String number) {
 		this.number = number;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
 	}
 }
